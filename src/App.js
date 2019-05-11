5f64825dbd8337edbd17b0df31218d3f7e64511c
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-05-11 11:06:30 -0300

    addung Source Sans Pro google font

--- src/App.js
+++ src/App.js
@@ -2,33 +2,28 @@
 
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+import injectSheet, { ThemeProvider } from "react-jss";
 
 library.add(faStroopwafel);
+
+const styles = {
+  '@global': {
+    body: {
+      fontFamily: "'Source Sans Pro', sans-serif"
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TITLE</h1>
         <FontAwesomeIcon icon="stroopwafel" />
-      </header>
     </div>
   );
 }
 
-export default App;
+export default injectSheet(styles)(App);
