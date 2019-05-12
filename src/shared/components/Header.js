bc221155d70f74deb9405f12b2fccd862d44eb59
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-05-12 11:36:32 -0300

    header title

--- src/shared/components/Header.js
+++ src/shared/components/Header.js
@@ -12,6 +12,35 @@ const styles = theme => ({
         height: theme.headerHeight,
         width: '100%',	
         background: theme.colors.lightGradient
+    },
+    headerContent: {
+      padding: '20px 16px 20px 25px'
+    },
+    titleContainer: {
+      color: '#3C4646',
+      '& h1': {
+        marginLeft: 17,
+        float: 'left',
+	      color: '#3C4646',	
+        fontSize: 20,	
+        '& span': {
+          fontWeight: 'bold'
+        }
+      },
+      '& svg': {
+        float: 'left',
+      }
+    },
+    editIcon: {
+      color: '#B1B6B6',	
+      fontSize: 14,
+      '& svg': {
+        marginLeft: 8,
+      }
+    },
+    userIcon: {
+      opacity: 0.5,
+      fontSize: 20
     }
 });
 
@@ -19,8 +48,17 @@ function Header(props: Props) {
 const { classes } = props;
   return (
     <header id="header" className={classes.mainHeader}>
-      <FontAwesomeIcon icon="user" />
-      <h1>Persona Capivara</h1>
+      <div className={classes.headerContent}>
+        <div className={classes.titleContainer}>
+          <div className={classes.userIcon}>
+            <FontAwesomeIcon icon="user" />
+          </div>
+          <h1>Persona <span>Capivara</span></h1>
+          <div className={classes.editIcon}>
+            <FontAwesomeIcon icon="edit" />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
