5fe59f45b14d7322b3512f7d231813bdbcb9d9a0
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-05-12 11:43:00 -0300

    extracting headertitle component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react';
 import injectSheet from "react-jss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeaderTitle from './components/HeaderTitle';
 
 type Props = {
   classes: any;
@@ -16,48 +16,14 @@ const styles = theme => ({
     headerContent: {
       padding: '20px 16px 20px 25px'
     },
-    titleContainer: {
-      color: '#3C4646',
-      '& h1': {
-        marginLeft: 17,
-        float: 'left',
-	      color: '#3C4646',	
-        fontSize: 20,	
-        '& span': {
-          fontWeight: 'bold'
-        }
-      },
-      '& svg': {
-        float: 'left',
-      }
-    },
-    editIcon: {
-      color: '#B1B6B6',	
-      fontSize: 14,
-      '& svg': {
-        marginLeft: 8,
-      }
-    },
-    userIcon: {
-      opacity: 0.5,
-      fontSize: 20
-    }
 });
 
 function Header(props: Props) {
-const { classes } = props;
+  const { classes } = props;
   return (
     <header id="header" className={classes.mainHeader}>
       <div className={classes.headerContent}>
-        <div className={classes.titleContainer}>
-          <div className={classes.userIcon}>
-            <FontAwesomeIcon icon="user" />
-          </div>
-          <h1>Persona <span>Capivara</span></h1>
-          <div className={classes.editIcon}>
-            <FontAwesomeIcon icon="edit" />
-          </div>
-        </div>
+        <HeaderTitle />
       </div>
     </header>
   );
