394bb6565149c1f1f55cea23e73a4b783656b31b
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-05-27 00:26:25 -0300

    Updates

---
+++ precache-manifest.554a9cab6bff823c78e3755386bb8974.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3541a16ac8681b282b7f589a5a9626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746e0a82abc28518b39a",
+    "url": "/static/css/main.0e28ef7f.chunk.css"
+  },
+  {
+    "revision": "edcdf8c2e69feabeca58",
+    "url": "/static/js/2.0d212c7b.chunk.js"
+  },
+  {
+    "revision": "746e0a82abc28518b39a",
+    "url": "/static/js/main.86190a62.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be86e94dbc304928a5584f980a54fc3c",
+    "url": "/static/media/beach.be86e94d.jpeg"
+  },
+  {
+    "revision": "8af3182fd246218f8305b1b5c3ce5605",
+    "url": "/static/media/capivara.8af3182f.jpg"
+  },
+  {
+    "revision": "3bbab165151edb41e78037fc6ba82e25",
+    "url": "/static/media/dog.3bbab165.jpg"
+  },
+  {
+    "revision": "0ac01ffa22f0eeefd8ed725d4271629a",
+    "url": "/static/media/tess.0ac01ffa.jpg"
+  }
+]);
