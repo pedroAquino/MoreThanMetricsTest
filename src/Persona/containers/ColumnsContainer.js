0084223d0e2a513788c7b9976721d9b36595166f
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-05-27 00:18:47 -0300

    dragging working

--- src/Persona/containers/ColumnsContainer.js
+++ src/Persona/containers/ColumnsContainer.js
@@ -2,20 +2,22 @@
 import React from 'react';
 import type { ColumnState } from '../services/columnDucks';
 import { connect } from 'react-redux';
-import { getColumns, updateField, removeField } from '../services/columnDucks';
+import { getColumns, updateField, removeField, addField } from '../services/columnDucks';
 import { DEFAULT_PERSONA_ID } from './PersonaContainer';
 import fieldFactory from '../services/fieldFactory';
 import type { Field } from '../services/fieldFactory';
 import { offset } from '../../shared/utils/DOMHelper';
-import type { DraggingState } from '../services/draggingDucks';
+import type { DraggingState } from '../../shared/services/draggingDucks';
 
 type Props = {
   children: any;
   columns: ColumnState;
   dragging: DraggingState;
+  personaRef: any;
   dispatchGet: (personaId: number) => void;
   dispatchUpdate: (field: Field) => void;
   dispatchRemoveField: (field: ?Field) => void;
+  dispatchAddField: (field: Field) => void;
 };
 
 type State = {
@@ -29,15 +31,19 @@ class ColumnsContainer extends React.Component<Props, State>{
         this.onRemoveField = this.onRemoveField.bind(this);
     }
 
+    containerPosition: number;
+
     componentDidMount() {
         this.props.dispatchGet(DEFAULT_PERSONA_ID);
     }
 
     componentDidUpdate(prevProps: Props) {
-        if (this.props.dragging.dragStatus === 'PARKED' &&
-            this.props.dragging.position
-        ) {
-            console.log('REF', offset(this.props.personaRef.current));
+        if (prevProps.dragging.dragStatus !== this.props.dragging.dragStatus && 
+            this.props.dragging.position) {
+            
+            const containerPosition = offset(this.props.personaRef.current).x + 664;
+            if (this.props.dragging.position.x < containerPosition)
+                this.props.dispatchAddField(this.props.dragging.field);
         }
     }
 
@@ -78,7 +84,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     dispatchGet: id => dispatch(getColumns(id)),
     dispatchUpdate: field => dispatch(updateField(field)),
-    dispatchRemoveField: field => dispatch(removeField(field))
+    dispatchRemoveField: field => dispatch(removeField(field)),
+    dispatchAddField: (field: Field) => dispatch(addField(field))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ColumnsContainer);
