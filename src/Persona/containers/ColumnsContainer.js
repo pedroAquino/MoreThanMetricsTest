99e80b81797f2846aa74294f43e55fa53a413420
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-05-26 21:03:02 -0300

    trying to solve dragginf problem

--- src/Persona/containers/ColumnsContainer.js
+++ src/Persona/containers/ColumnsContainer.js
@@ -6,10 +6,13 @@ import { getColumns, updateField, removeField } from '../services/columnDucks';
 import { DEFAULT_PERSONA_ID } from './PersonaContainer';
 import fieldFactory from '../services/fieldFactory';
 import type { Field } from '../services/fieldFactory';
+import { offset } from '../../shared/utils/DOMHelper';
+import type { DraggingState } from '../services/draggingDucks';
 
 type Props = {
   children: any;
   columns: ColumnState;
+  dragging: DraggingState;
   dispatchGet: (personaId: number) => void;
   dispatchUpdate: (field: Field) => void;
   dispatchRemoveField: (field: ?Field) => void;
@@ -26,42 +29,50 @@ class ColumnsContainer extends React.Component<Props, State>{
         this.onRemoveField = this.onRemoveField.bind(this);
     }
 
+    componentDidMount() {
+        this.props.dispatchGet(DEFAULT_PERSONA_ID);
+    }
 
-  componentDidMount() {
-      this.props.dispatchGet(DEFAULT_PERSONA_ID);
-  }
+    componentDidUpdate(prevProps: Props) {
+        if (this.props.dragging.dragStatus === 'PARKED' &&
+            this.props.dragging.position
+        ) {
+            console.log('REF', offset(this.props.personaRef.current));
+        }
+    }
 
-  /*:: onUpdateFeld: () => void */
-  onUpdateFeld(field: any) {
-      const parsed = fieldFactory({
-          title: 'info',
-          data: field.info
-      });
-      this.props.dispatchUpdate(parsed);
-  }
+    /*:: onUpdateFeld: () => void */
+    onUpdateFeld(field: any) {
+        const parsed = fieldFactory({
+            title: 'info',
+            data: field.info
+        });
+        this.props.dispatchUpdate(parsed);
+    }
 
-  /*:: onRemoveField: () => void */
-  onRemoveField(fieldId: number) {
-      const field = this.props.columns.items
+    /*:: onRemoveField: () => void */
+    onRemoveField(fieldId: number) {
+        const field = this.props.columns.items
         .map(item => item.fields)
         .reduce((acc, value) => acc.concat(value), [])
         .find(f => f.id === fieldId);
-    console.log(field);
-     this.props.dispatchRemoveField(field);
-  }
-  
-  render() {
-      return this.props.children(
-          this.props.columns,
-          this.onUpdateFeld,
-          this.onRemoveField
-      );
-  }
+
+        this.props.dispatchRemoveField(field);
+    }
+
+    render() {
+        return this.props.children(
+            this.props.columns,
+            this.onUpdateFeld,
+            this.onRemoveField
+        )
+    }
 
 };
 
 const mapStateToProps = state => ({
-    columns: state.entities.columns
+    columns: state.entities.columns,
+    dragging: state.ui.dragging
 });
 
 const mapDispatchToProps = dispatch => ({
---
+++ src/shared/utils/DOMHelper.js
@@ -0,0 +1,7 @@
+
+export function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { x: rect.top + scrollTop, y: rect.left + scrollLeft }
+}
